7fecc9c4e13a633994b0c30abc7f2eb14ab1da78
User-081 <sonntagsmailbox@gmail.com>
2026-03-28 17:41:21 +0100

    Fix: use pointer references

--- components/amis/meter.cpp
+++ components/amis/meter.cpp
@@ -74,24 +74,24 @@ namespace esphome
         /* clients events */
         static void handleError(void *arg, AsyncClient *client, int8_t error)
         {
-            ESP_LOGE(TAG, "[Fronius] connection error %s from client %s \n", client->errorToString(error),  ip4addr_ntoa(client->getRemoteAddress4()));
+            ESP_LOGE(TAG, "[Fronius] connection error %s from client %s \n", client->errorToString(error),  ip4addr_ntoa(&client->getRemoteAddress4()));
         }
 
         static void handleDisconnect(void *arg, AsyncClient *client)
         {
-            ESP_LOGI(TAG, "[Fronius] client %s disconnected \n",  ip4addr_ntoa(client->getRemoteAddress4()));
+            ESP_LOGI(TAG, "[Fronius] client %s disconnected \n",  ip4addr_ntoa(&client->getRemoteAddress4()));
         }
 
         static void handleTimeOut(void *arg, AsyncClient *client, uint32_t time)
         {
-            ESP_LOGW(TAG, "[Fronius] client ACK timeout ip: %s \n", ip4addr_ntoa(client->getRemoteAddress4()));
+            ESP_LOGW(TAG, "[Fronius] client ACK timeout ip: %s \n", ip4addr_ntoa(&client->getRemoteAddress4()));
         }
 
         static void handleData(void *arg, AsyncClient *client, void *data, size_t len);
 
         static void handleNewClient(void *arg, AsyncClient *client)
         {
-            ESP_LOGI(TAG, "[Fronius] new client has been connected to server, ip: %s\n", ip4addr_ntoa(client->getRemoteAddress4()));
+            ESP_LOGI(TAG, "[Fronius] new client has been connected to server, ip: %s\n", ip4addr_ntoa(&client->getRemoteAddress4()));
 
             // register events
             client->onData(&handleData, NULL);
@@ -107,7 +107,7 @@ namespace esphome
 
         static void handleData(void *arg, AsyncClient *client, void *data, size_t len)
         {
-            ESP_LOGD(TAG, "[Fronius] Poll IP:%s\n",  ip4addr_ntoa(client->getRemoteAddress4()));
+            ESP_LOGD(TAG, "[Fronius] Poll IP:%s\n",  ip4addr_ntoa(&client->getRemoteAddress4()));
             if (!isDataAvailable())
                 return; // erst beantworten wenn Zählerdaten vorhanden
             memcpy(mHeader, data, len < sizeof(mHeader) ? len : sizeof(mHeader));
